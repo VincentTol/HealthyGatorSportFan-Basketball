9457aed5e9758c73a8b74c3c2286931c49442f18
Gabriel Lemus <lemusg@ufl.edu>
2026-04-09 21:14:13 -0400

    Quick Fixes
    Fixed merge error in progresslogging.tsx and corrected .gitignore to correctly skip AppUrls.ts and google-services.json

--- HealthyGatorSportsFanRN/constants/AppUrls.ts
+++ HealthyGatorSportsFanRN/constants/AppUrls.ts
@@ -1,6 +1,6 @@
 export const AppUrls = {
   /** The base URL for the API. Make sure to update this if the ngrok URL changes and put https at the beginning   */
-  url: 'https://nonparabolical-unwaddling-blaise.ngrok-free.dev',
+  url: 'https://interprofessionally-nonappeasable-garfield.ngrok-free.dev',
   /** Required for ngrok free tier: skip the "Visit Site" HTML page so API returns JSON */
   apiHeaders: { 'ngrok-skip-browser-warning': 'true' as const },
 };
